3b1fd1962788601db47fa1d6f63d5216a5b8c630
Ayelen Marina Sarco <ayelensarco94@gmail.com>
2026-01-12 16:38:17 -0300

    Initial project setup

---
+++ src/main/java/com/example/retail/retail_management_system/RetailManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.retail.retail_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailManagementSystemApplication.class, args);
+	}
+
+}
