a7a0499bd8bae1e6d2dce408ae33b31c06ab30fb
Ayelen Marina Sarco <ayelensarco94@gmail.com>
2026-01-14 12:08:20 -0300

    update dtos

--- src/main/java/com/example/retail/retail_management_system/dto/ProductDTO.java
+++ src/main/java/com/example/retail/retail_management_system/dto/ProductDTO.java
@@ -1,6 +1,8 @@
 package com.example.retail.retail_management_system.dto;
 
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Getter @Setter
@@ -9,10 +11,13 @@ import lombok.*;
 @Builder
 public class ProductDTO {
 
-    private Long productId;
+    private Long id;
+    @NotBlank(message = "Product name required.")
     private String name;
     private String brand;
+    @NotNull(message = "Current price required.")
     private Double currentPrice;
-    private int stock;
+    @NotNull(message="Stock required.")
+    private Integer stock;
 
 }
