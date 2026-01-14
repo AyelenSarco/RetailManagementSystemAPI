3f80ffc0ba107019f2d9a34436fd4718cbc810c6
Ayelen Marina Sarco <ayelensarco94@gmail.com>
2026-01-14 12:11:04 -0300

    update mappers

--- src/main/java/com/example/retail/retail_management_system/mapper/ProductMapper.java
+++ src/main/java/com/example/retail/retail_management_system/mapper/ProductMapper.java
@@ -3,11 +3,13 @@ package com.example.retail.retail_management_system.mapper;
 import com.example.retail.retail_management_system.dto.ProductDTO;
 import com.example.retail.retail_management_system.model.Product;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
 
-
+    @Mapping(target="id", source="productId")
     ProductDTO toDTO(Product product);
+
     Product toEntity(ProductDTO productDTO);
 }
--- src/main/java/com/example/retail/retail_management_system/mapper/SaleDetailMapper.java
+++ src/main/java/com/example/retail/retail_management_system/mapper/SaleDetailMapper.java
@@ -4,10 +4,17 @@ package com.example.retail.retail_management_system.mapper;
 import com.example.retail.retail_management_system.dto.SaleDetailDTO;
 import com.example.retail.retail_management_system.model.SaleDetail;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface SaleDetailMapper {
 
+    @Mapping(target="productId", source="product.productId")
+    @Mapping(target="productName", source ="product.name")
     SaleDetailDTO toDTO(SaleDetail saleDetail);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "product", ignore = true)
+    @Mapping(target = "sale", ignore = true)
     SaleDetail toEntity(SaleDetailDTO saleDetailDTO);
 }
